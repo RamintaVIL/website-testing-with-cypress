8f5e7105a441d41dd46a16cc5a66d186045ee14d
Raminta <1989sliekiukasss@gmail.com>
2024-10-01 13:50:27 +0300

    details test in form

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -37,4 +37,22 @@ describe('User registration flow', () => {
     cy.contains("h2", "Enter Account Information").should('be.visible');
   });
 
+  it('should allow to fill details: should display the "Enter Account Information" page after signup', () => {
+    cy.get("a[href='/login']").click();
+    cy.get('[data-qa="signup-name"]').should('be.visible').type('Raminta');
+    cy.get('[data-qa="signup-email"]').should('be.visible').type('Raminta@blabla.com');
+    cy.contains("button", "Signup").click();
+    cy.get('#id_gender2').should('be.visible').check();
+    cy.get('#name').should('be.visible').type('Raminta');
+
+    // cy.get('#email', { timeout: 80000 }) // Wait up to 10 seconds for the email field
+    //   .should('be.visible')
+    //   .should('not.be.disabled')
+    //   .type('Raminta@blabla.com');
+
+    cy.get('#password').should('be.visible').type('1a1a1aqaaa+++');
+    cy.get('#days').should('be.visible').select('12');
+    cy.get('#months').should('be.visible').select('September');
+    cy.get('#years').should('be.visible').select('1989');
+  })
 })
