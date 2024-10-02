2fa3a684e47cc997678acf7d48af19c7c88d6624
Raminta <1989sliekiukasss@gmail.com>
2024-10-02 22:52:08 +0300

    add comments about contact us form tests

--- cypress/e2e/contactUsForm.cy.js
+++ cypress/e2e/contactUsForm.cy.js
@@ -1,26 +1,33 @@
 it('should load contact us form and let it fill', () => {
-  // Launch browser
   cy.visit("");
+
   // Verify that home page is visible successfully
   cy.get('body').should('be.visible');
+
   // Click on 'Contact Us' button
   cy.get('a[href="/contact_us"]').should('contain', 'Contact us').click();
+
   // Verify 'GET IN TOUCH' is visible
   cy.contains('Get In Touch').should('be.visible');
+
   // Enter name, email, subject and message
   cy.get('[data-qa="name"]').type('Raminta');
   cy.get('[data-qa="email"]').type('Raminta@blabla');
   cy.get('[data-qa="subject"]').type("aaaaaaa");
   cy.get('[data-qa="message"]').type("asdjasdksjaflksajfklsafjlsfjsl");
+
   // Upload file
   cy.get('input[name="upload_file"]').selectFile('cypress/fixtures/example.json');
   cy.get('input[data-qa="submit-button"]').click();
+
   // Click OK button (galite naudoti cy.on() norint užfiksuoti alert)
   cy.on('window:alert', (str) => {
     expect(str).to.equal('Your details have been submitted successfully.');
   });
+
   // Verify success message is visible
   cy.contains('Success! Your details have been submitted successfully.').should('be.visible');
+
   // Click 'Home' button and verify that landed to home page successfully
   cy.get('.btn.btn-success').should('contain', 'Home').click();
   cy.get('body').should('be.visible');
