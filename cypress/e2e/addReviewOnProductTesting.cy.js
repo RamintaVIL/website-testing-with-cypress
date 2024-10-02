c2eb1e123228902353f5a0d4ec8b940582af66ff
Raminta <1989sliekiukasss@gmail.com>
2024-10-02 22:45:23 +0300

    add comments in add review test

--- cypress/e2e/addReviewOnProductTesting.cy.js
+++ cypress/e2e/addReviewOnProductTesting.cy.js
@@ -1,13 +1,25 @@
 it('should allow user to add a review to a product', () => {
   cy.visit("");
-  cy.get('a[href="/products"]').click();
+
+  // Click on 'Products' button
+  cy.contains('Products').click();
+  // cy.get('a[href="/products"]').click();
+
+  // Verify user is navigated to ALL PRODUCTS page successfully
   cy.contains('All Products').should('be.visible');
+
+  // Click on 'View Product' button
   cy.get('a[href="/product_details/1"]').click();
-  cy.contains('Write Your Review').should('be.visible');
+
+  // Enter name, email and review
   cy.get('input#name').type('jdnk');
   cy.get('#email').type('jdnk@dsjs.lt');
   cy.get('#review').type('sdfjdhsfjhdsfiuhefiuehfuehf');
+
+  // Click 'Submit' button
   cy.get('#button-review').click();
+
+  // Verify success message 'Thank you for your review.' is visible
   cy.contains('Thank you for your review').should('be.visible');
 });
 
