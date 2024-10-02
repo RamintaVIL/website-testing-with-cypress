8671151dbb672c47b46d37022b31c27040264206
Raminta <1989sliekiukasss@gmail.com>
2024-10-02 22:59:57 +0300

    comments about product searching

--- cypress/e2e/productSearch.cy.js
+++ cypress/e2e/productSearch.cy.js
@@ -1,17 +1,21 @@
-it('should load test cases page', () => {
+it('should load search results for a product', () => {
   cy.visit("");
   cy.get('body').should('be.visible');
 
+  // Click on 'Products' button
   cy.get("a[href='/products']").should("contain", "Products").click();
+
+  // Verify user is navigated to ALL PRODUCTS page successfully
   cy.contains('All Products').should('be.visible');
 
+  // Enter product name in search input and click search button
   cy.get('input[name="search"]').type('Jeans');
   cy.get('button[type="button"]').click();
 
+  // Verify 'SEARCHED PRODUCTS' is visible
   cy.contains('Searched Products').should('be.visible');
-  cy.get('a[href="#Men"]').click();
-  cy.get('a[href="/category_products/6"]').type('Jeans');
 
+  // Verify all the products related to search are visible
   cy.get('.features_items')
     .should('have.length.greaterThan', 0)
     .each(($el) => {
