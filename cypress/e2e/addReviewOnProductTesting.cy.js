4a3523fa5b49b2dedc0a250d6051d40044f94ed7
Raminta <1989sliekiukasss@gmail.com>
2024-10-03 13:38:19 +0300

    create review data

--- cypress/e2e/addReviewOnProductTesting.cy.js
+++ cypress/e2e/addReviewOnProductTesting.cy.js
@@ -3,13 +3,15 @@ it('should allow the user to add a review for a product', () => {
   cy.navigateToProductsPage();
   cy.navigateToProductDetails();
 
-  // Enter name, email and review
-  cy.get('input#name').type('jdnk');
-  cy.get('#email').type('jdnk@dsjs.lt');
-  cy.get('#review').type('sdfjdhsfjhdsfiuhefiuehfuehf');
+  cy.fixture('reviewData').then((data) => {
+    // Enter name, email and review using the data from fixture
+    cy.get('input#name').type(data.name);
+    cy.get('#email').type(data.email);
+    cy.get('#review').type(data.review);
 
-  // Click 'Submit' button
-  cy.get('#button-review').click();
+    // Click 'Submit' button
+    cy.get('#button-review').click();
+  });
 
   // Verify success message 'Thank you for your review.' is visible
   cy.contains('Thank you for your review').should('be.visible');
