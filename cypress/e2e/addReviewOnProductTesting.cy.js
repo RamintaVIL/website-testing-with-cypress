f5c822c443d53cd492bbc58f3e12155849762b67
Raminta <1989sliekiukasss@gmail.com>
2024-10-03 14:48:25 +0300

    find mistake

--- cypress/e2e/addReviewOnProductTesting.cy.js
+++ cypress/e2e/addReviewOnProductTesting.cy.js
@@ -5,7 +5,7 @@ it('should allow the user to add a review for a product', () => {
 
   cy.fixture('reviewData').then((data) => {
     // Enter name, email and review using the data from fixture
-    cy.get('input#name').type(data.name);
+    cy.get('#name').type(data.name);
     cy.get('#email').type(data.email);
     cy.get('#review').type(data.review);
 
