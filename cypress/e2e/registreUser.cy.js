fd10ea3c6b7cc1957fdf611c1d6bdd15296fd58c
Raminta <1989sliekiukasss@gmail.com>
2024-10-03 14:58:13 +0300

    create command

--- cypress/e2e/registreUser.cy.js
+++ cypress/e2e/registreUser.cy.js
@@ -19,29 +19,7 @@ it('should register a user successfully and delete the account ', () => {
 
     // Verify 'Enter Account Information' is visible
     cy.contains("h2", "Enter Account Information").should('be.visible');
-
-    // Fill out account details
-    cy.get('#id_gender2').check();
-    cy.get('#password').type(data.password);
-    cy.get('#days').select('12');
-    cy.get('#months').select('September');
-    cy.get('#years').select('1989');
-
-    // Select checkboxes for newsletters and offers
-    cy.get('#newsletter').check();
-    cy.get('#optin').check();
-
-    // Fill out additional details
-    cy.get('#first_name').type(data.first_name);
-    cy.get('#last_name').type(data.last_name);
-    cy.get('#company').type(data.company);
-    cy.get('#address1').type(data.address1);
-    cy.get('#address2').type(data.address2);
-    cy.get('#country').select(data.country);
-    cy.get('#state').type(data.state);
-    cy.get('#city').type(data.city);
-    cy.get('#zipcode').type(data.zipcode);
-    cy.get('#mobile_number').type(data.mobile_number);
+    cy.fillOutAccountDetails(data);
 
     // Click 'Create Account' button and verify 'Account Created!' message
     cy.contains("button", "Create Account").click();
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,3 +24,28 @@ Cypress.Commands.add('navigateToContactUsPage', () => {
     // Verify 'GET IN TOUCH' is visible
     cy.contains('Get In Touch').should('be.visible');
 });
+
+Cypress.Commands.add('fillOutAccountDetails', (data) => {
+    cy.get('#id_gender2').check();
+    cy.get('#password').type(data.password);
+    cy.get('#days').select('12');
+    cy.get('#months').select('September');
+    cy.get('#years').select('1989');
+
+    // Select checkboxes for newsletters and offers
+    cy.get('#newsletter').check();
+    cy.get('#optin').check();
+
+    // Fill out additional details
+    cy.get('#first_name').type(data.first_name);
+    cy.get('#last_name').type(data.last_name);
+    cy.get('#company').type(data.company);
+    cy.get('#address1').type(data.address1);
+    cy.get('#address2').type(data.address2);
+    cy.get('#country').select(data.country);
+    cy.get('#state').type(data.state);
+    cy.get('#city').type(data.city);
+    cy.get('#zipcode').type(data.zipcode);
+    cy.get('#mobile_number').type(data.mobile_number);
+});
+
