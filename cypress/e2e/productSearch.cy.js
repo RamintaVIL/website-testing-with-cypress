061ab62e72020e340110ae7e65a038f035fc0c1e
Raminta <1989sliekiukasss@gmail.com>
2024-10-03 12:08:48 +0300

    create command and used

--- cypress/e2e/productSearch.cy.js
+++ cypress/e2e/productSearch.cy.js
@@ -1,12 +1,6 @@
 it('should load search results for a product', () => {
   cy.visit("");
-  cy.get('body').should('be.visible');
-
-  // Click on 'Products' button
-  cy.get("a[href='/products']").should("contain", "Products").click();
-
-  // Verify user is navigated to ALL PRODUCTS page successfully
-  cy.contains('All Products').should('be.visible');
+  cy.navigateToProductsPage();
 
   // Enter product name in search input and click search button
   cy.get('input[name="search"]').type('Jeans');
